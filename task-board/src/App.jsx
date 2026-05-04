3dbb701ae3a210d617768da3d8a8dca02d04b139
Marcos <marcos.garrido.dev@gmail.com>
2026-05-04 12:09:48 +0200

    Create project architecture

--- task-board/src/App.jsx
+++ task-board/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+import { initialTasks } from "./data/initialTasks"
+
 function App() {
+  const [tasks, setTasks] = useState(initialTasks)
+  
+  console.log(tasks)
+
   return (
     <h1>Task Board</h1>
   )
---
+++ task-board/src/data/initialTasks.js
@@ -0,0 +1,5 @@
+export const initialTasks = {
+  todo: [],
+  doing: [],
+  done: []
+}
